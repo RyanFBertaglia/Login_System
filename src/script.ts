e7e8780bbe7fd9ae973a72b7d4abd27a15fa61d2
Ryan-Bertaglia1 <ryanfernandesbertaglia@gmail.com>
2024-08-17 15:49:43 -0300

    Conexão com a API e teste de envio

--- src/script.ts
+++ src/script.ts
@@ -1,21 +1,47 @@
+// Abre a rota pública
 import mongoose from "mongoose";
 import express, { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+
+
+
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+//import User from './model/User';
+import User from './User';
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
-// Abre a rota pública
-async function opennet() {
+
     app.get('/', (req: Request, res: Response) => {
         res.status(200).json({ msg: 'Bem vindo ao Banco de Dados' });
     });
+    app.post("/auth/register", async (req: Request, res: Response)=> {
+        const {name, email, senha} = req.body
 
+    //validações de dados
+    if(!name){
+        return res.status(422).json({msg: "O nome é obrigatório"})
+    }
+    if(!email){
+        return res.status(422).json({msg: "O email é obrigatório"})
+    }
+    if(!senha){
+        return res.status(422).json({msg: "A senha é obrigatória"})
+    }
+    })
+    const novoUsuario = new User({
+        nome: 'Ryan',
+        email: 'ryan@gmail.com',
+        senha: 'rakjf'
+    })
+    novoUsuario.save()
+        .then(() =>
+        console.log('Usuario salvo'));
     try {
-        await mongoose.connect(
+        mongoose.connect(
             `mongodb+srv://${dbUser}:${dbPassword}@dblogin.cvr34.mongodb.net/?retryWrites=true&w=majority&appName=DBLOGIN`
         );
         app.listen(3000, () => {
@@ -25,6 +51,3 @@ async function opennet() {
     } catch (err) {
         console.error('Erro ao conectar ao banco: ', err);
     }
-}
-
-opennet();
